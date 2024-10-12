c751276c95501dc81dcbc638cd86d35ddaa43594
lingfish <jason@lucid.net.au>
2024-10-12 14:27:22 +1100

    Add 'human' CLI option
    Another fairly major code refactor, and add the 'human' CLI option.
    
    Changelog: added

--- src/proxmox_info/pminfo.py
+++ src/proxmox_info/pminfo.py
@@ -12,6 +12,7 @@ import pandas as pd
 import humanize
 import click
 from rich.console import Console, Group
+from rich.status import Status
 from rich.table import Table
 from rich import box
 from rich import inspect
@@ -26,38 +27,26 @@ from ._version import __version__
 pd.options.mode.copy_on_write = True
 install(show_locals=True)
 
-def machines_by_storage(current_node: proxmoxer.ProxmoxResource, current_storage: dict) -> pandas.DataFrame:
-    """
-    Finds all machines in a specified storage.
 
-    :param current_node:  The current Proxmox node.
-    :type current_node:  proxmoxer.ProxmoxResource
-    :param current_storage: The current storage to inspect.
-    :type current_storage: dict
-    :return: The machines running on the specified storage.
-    :rtype: dict
-    """
-    df = pd.DataFrame(current_node.storage(current_storage['storage']).content.get())
+def machines_by_storage(current_node: proxmoxer.ProxmoxResource, current_storage: str) -> pandas.DataFrame:
+    """Finds all machines in a specified storage."""
+
+    df = pd.DataFrame(current_node.storage(current_storage).content.get())
     return df[df['content'].isin(['images', 'rootdir'])]
 
 
-def rejig_machines(machines: pandas.DataFrame) -> pandas.DataFrame:
-    """
-    This does a bunch of filtering and reorganising of machines to wittle down to the desired columns etc.
+def rejig_machines(machines: pandas.DataFrame, human: bool) -> pandas.DataFrame:
+    """This does a bunch of filtering and reorganising of machines to wittle down to the desired columns etc."""
 
-    :param machines: A Pandas DataFrame object
-    :type machines: pandas.DataFrame
-    :return: A Pandas DataFrame object
-    :rtype: pandas.DataFrame
-    """
     machines.drop(columns=['diskread', 'diskwrite', 'cpu', 'disk', 'swap', 'type'], errors='ignore', inplace=True)
-    for k in ['maxdisk', 'maxmem', 'mem', 'netout', 'netin', 'maxswap']:
-        try:
-            machines[k] = machines[k].map(humanize.naturalsize)
-        except KeyError:
-            pass
+    if human:
+        for k in ['maxdisk', 'maxmem', 'mem', 'netout', 'netin', 'maxswap']:
+            try:
+                machines[k] = machines[k].map(humanize.naturalsize)
+            except KeyError:
+                pass
 
-    machines['uptime'] = machines['uptime'].map(humanize.naturaltime)
+        machines['uptime'] = machines['uptime'].map(humanize.naturaltime)
     machines['pid'] = machines['pid'].astype(int)
     machines.sort_index(axis=1, inplace=True)
     left_columns = ['name', 'vmid', 'status']
@@ -67,25 +56,14 @@ def rejig_machines(machines: pandas.DataFrame) -> pandas.DataFrame:
     return machines
 
 
+def df_to_table(pandas_dataframe: pandas.DataFrame,
+                rich_table: rich.table.Table,
+                show_index: bool = True,
+                index_name: Optional[str] = None,
+                col_align_map: Optional[dict] = None
+    ) -> rich.table.Table:
+    """Convert a pandas.DataFrame object into a rich.Table object."""
 
-def df_to_table(pandas_dataframe: pandas.DataFrame, rich_table: rich.table.Table, show_index: bool = True,
-                index_name: Optional[str] = None, col_align_map: Optional[dict] = None) -> rich.table.Table:
-    """
-    Convert a pandas.DataFrame object into a rich.Table object.
-
-    :param pandas_dataframe: A Pandas DataFrame to be converted to a rich Table.
-    :type pandas_dataframe: pandas.DataFrame
-    :param rich_table: A rich Table that should be populated by the DataFrame values.
-    :type rich_table: rich.table.Table
-    :param show_index: Add a column with a row count to the table. Defaults to True.
-    :type show_index: bool
-    :param index_name: The column name to give to the index column. Defaults to None, showing no value.
-    :type index_name: str
-    :param col_align_map: A map of columns describing alignment.
-    :type col_align_map: dict
-    :return:The rich Table instance passed, populated with the DataFrame values.
-    :rtype: rich.table.Table
-    """
     if show_index:
         index_name = str(index_name) if index_name else ""
         rich_table.add_column(index_name)
@@ -108,8 +86,28 @@ def df_to_table(pandas_dataframe: pandas.DataFrame, rich_table: rich.table.Table
 
 
 class MachineType(Enum):
+    """Helper Enum"""
+
     VMs = 'Virtual machines'
     LXCs = "Linux containers"
+
+
+def fetch_node_info(proxmox: ProxmoxAPI,
+                    node: proxmoxer.ProxmoxResource,
+                    console: Console
+    ) -> tuple[proxmoxer.ProxmoxResource, proxmoxer.ProxmoxResource, Status]:
+    """Fetches node information."""
+
+    with console.status('Fetching info', spinner='dots10') as status:
+        current_node = proxmox.nodes(node['node'])
+        yield node['node'], current_node, status
+
+
+def fetch_storage_info(current_node: proxmoxer.ProxmoxResource, storage: str) -> str:
+    """Fetches storage information."""
+    for current_storage in current_node.storage.get(content='images,rootdir'):
+        if storage == 'all' or current_storage['storage'] == storage:
+            yield current_storage['storage']
 
 
 @click.command()
@@ -122,8 +120,9 @@ class MachineType(Enum):
 @click.option('--output', '-o', default='basic', help='The output format: basic or tree')
 @click.option('--filter', '-f', default='running', help='Status of machines to filter: running or all')
 @click.option('--pager/--no-pager', '-l', default=False, help='Run the output through the system pager')
+@click.option('--human/--no-human', default=True, help='Show human-readable formatted output')
 @click.version_option(version=__version__)
-def main(host, user, password, verify, timeout, storage, output, filter, pager):
+def main(host, user, password, verify, timeout, storage, output, filter, pager, human):
     validation_messages = {
         'must_exist_true': '{name} is required',
         'condition': '{name} is required',
@@ -183,59 +182,61 @@ def main(host, user, password, verify, timeout, storage, output, filter, pager):
     }
 
     for node in proxmox.nodes.get():
-        with console.status('Fetching info', spinner='dots10') as status:
-            current_node = proxmox.nodes(node['node'])
+        for node_info, current_node, status in fetch_node_info(proxmox, node, console):
             if output == 'basic':
-                console.print(f'[bold green]Node: {node["node"]}')
-                console.print()
+                console.print(f'[bold green]Node: {node_info}')
             elif output == 'tree':
-                tree = Tree(f'Node: {node["node"]}')
+                tree = Tree(f'Node: {node_info}')
 
-            for current_storage in current_node.storage.get(content='images,rootdir'):
+            for current_storage in fetch_storage_info(current_node, storage):
                 try:
-                    status.update(f'Fetching storage: {current_storage["storage"]}')
-                    if storage == 'all' or current_storage['storage'] == storage:
+                    status.update(f'Fetching storage: {current_storage}')
+                    if output == 'basic':
+                        console.rule(f'[green]Storage: {current_storage}', align='left')
+                    elif output == 'tree':
+                        tree_storage = tree.add(f'[green]Storage: {current_storage}')
+
+                    machines = machines_by_storage(current_node, current_storage)
+                    if machines.empty:
+                        msg = f'[bright_yellow]Nothing applicable found'
                         if output == 'basic':
-                            console.rule(f'[green]Storage: {current_storage["storage"]}', align='left')
+                            console.print(msg)
                         elif output == 'tree':
-                            tree_storage = tree.add(f'[green]Storage: {current_storage["storage"]}')
+                            tree_storage.add(msg)
+                        continue
 
-                        machines = machines_by_storage(current_node, current_storage)
-                        if machines.empty:
-                            continue
+                    for container_type in MachineType:
+                        status.update(f'Fetching storage: {current_storage} :right_arrow: {container_type.name}')
+                        if container_type == MachineType.VMs:
+                            df = pd.DataFrame(current_node.qemu.get())
+                        elif container_type == MachineType.LXCs:
+                            df = pd.DataFrame(current_node.lxc.get())
+                        df = df[df['vmid'].isin(machines['vmid'])]
 
-                        for container_type in MachineType:
-                            status.update(f'Fetching storage: {current_storage["storage"]} :right_arrow: {container_type.name}')
-                            if container_type == MachineType.VMs:
-                                df = pd.DataFrame(current_node.qemu.get())
-                            elif container_type == MachineType.LXCs:
-                                df = pd.DataFrame(current_node.lxc.get())
-                            df = df[df['vmid'].isin(machines['vmid'])]
+                        if not df.empty:
+                            final_machines = rejig_machines(df[df['status'] == filter] if filter == 'running' else df, human)
+                        else:
+                            final_machines = pd.DataFrame()
 
-                            if not df.empty:
-                                final_machines = rejig_machines(df[df['status'] == filter] if filter == 'running' else df)
-                            else:
-                                final_machines = pd.DataFrame()
+                        if not final_machines.empty:
+                            table = Table(title=container_type.value, show_header=True, header_style='on grey19',
+                                          box=box.MINIMAL_HEAVY_HEAD, title_style='reverse')
+                            table = df_to_table(final_machines, table, show_index=False, col_align_map=col_align[container_type])
 
-                            if not final_machines.empty:
-                                table = Table(title=container_type.value, show_header=True, header_style='on grey19',
-                                              box=box.MINIMAL_HEAVY_HEAD, title_style='reverse')
-                                table = df_to_table(final_machines, table, show_index=False, col_align_map=col_align[container_type])
+                            if output == 'basic':
+                                console.print(f'\n[bright_yellow]:computer: {container_type.name}: {" ".join(str(x) for x in final_machines["vmid"].to_list())}')
+                                console.print(table)
+                            elif output == 'tree':
+                                tree_storage.add(Group(f':computer: {container_type.name} :arrow_lower_right:', table))
+                        else:
+                            msg = f'[bright_yellow]No {container_type.name} found'
+                            if output == 'basic':
+                                console.print(msg)
+                            elif output == 'tree':
+                                tree_storage.add(Group(f':computer: {container_type.name} :arrow_lower_right:', msg))
 
-                                if output == 'basic':
-                                    console.print(f'\n[bright_yellow]:computer: {container_type.name}: {" ".join(str(x) for x in final_machines["vmid"].to_list())}')
-                                    console.print(table)
-                                elif output == 'tree':
-                                    tree_storage.add(Group(f':computer: {container_type.name} :arrow_lower_right:', table))
-                            else:
-                                msg = f'[bright_yellow]No {container_type.name} found'
-                                if output == 'basic':
-                                    console.print(msg)
-                                elif output == 'tree':
-                                    tree_storage.add(Group(f':computer: {container_type.name} :arrow_lower_right:', msg))
-
-                        if output == 'basic':
-                            console.print()
+                    if output == 'basic':
+                        console.print()
 
                 except proxmoxer.core.ResourceException:
                     print('Couldn\'t get datastores, moving on...')
